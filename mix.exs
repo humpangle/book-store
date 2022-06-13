4c7d931d76e9586fababcef4a49661c7f98062b1
kanmii <maneptha@gmail.com>
2022-06-13 04:08:04 +0200

    Added os_mon, httpoison and remove floki as test dependency only

--- mix.exs
+++ mix.exs
@@ -20,7 +20,11 @@ defmodule BookStore.MixProject do
   def application do
     [
       mod: {BookStore.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [
+        :logger,
+        :runtime_tools,
+        :os_mon
+      ]
     ]
   end
 
@@ -40,7 +44,7 @@ defmodule BookStore.MixProject do
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.17.5"},
-      {:floki, ">= 0.30.0", only: :test},
+      {:floki, ">= 0.30.0"},
       {:phoenix_live_dashboard, "~> 0.6"},
       {:esbuild, "~> 0.4", runtime: Mix.env() == :dev},
       {:swoosh, "~> 1.3"},
@@ -48,7 +52,8 @@ defmodule BookStore.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:httpoison, "~> 1.6"}
     ]
   end
 
