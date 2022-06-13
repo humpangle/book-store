e3b37072aab5d4cac67c4cbd7135e41756fbdcc6
kanmii <maneptha@gmail.com>
2022-06-13 09:00:55 +0200

    Extract book links from manning page catalog

--- lib/book_store/manning_book_scraper.ex
+++ lib/book_store/manning_book_scraper.ex
@@ -1,5 +1,6 @@
 defmodule BookStore.ManningBookScraper do
-  @url "https://www.manning.com/catalog"
+  @manning_host "www.manning.com"
+  @url "https://#{@manning_host}/catalog"
 
   @poison_opts [
     timeout: :timer.seconds(100),
@@ -8,8 +9,22 @@ defmodule BookStore.ManningBookScraper do
 
   def get_manning_books() do
     get_manning_catalog_page()
+    |> get_book_links_from_page()
   end
 
   defp get_manning_catalog_page,
     do: HTTPoison.get!(@url, [], @poison_opts).body
+
+  defp get_book_links_from_page(page_source) do
+    page_source
+    |> Floki.parse_document!()
+    |> Floki.find("a.catalog-link")
+    |> Enum.map(
+      &URI.to_string(%URI{
+        path: Floki.attribute(&1, "href"),
+        host: @manning_host,
+        scheme: "https"
+      })
+    )
+  end
 end
