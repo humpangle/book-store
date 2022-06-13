586b68d0d9be8627c9c8f8079fe2cf0e9a2e8337
kanmii <maneptha@gmail.com>
2022-06-13 11:18:55 +0200

    wip

--- lib/book_store/manning_book_scraper.ex
+++ lib/book_store/manning_book_scraper.ex
@@ -10,6 +10,8 @@ defmodule BookStore.ManningBookScraper do
   def get_manning_books() do
     get_manning_catalog_page()
     |> get_book_links_from_page()
+    |> Task.async_stream(&get_book_details/1)
+    |> Task.await(:timer.seconds(2000))
   end
 
   defp get_manning_catalog_page,
@@ -27,4 +29,50 @@ defmodule BookStore.ManningBookScraper do
       })
     )
   end
+
+  defp get_book_details(book_url) do
+    parsed_page =
+      HTTPoison.get!(book_url, @poison_opts).body
+      |> Floki.parse_document!()
+
+    %{
+      title: get_book_title(parsed_page),
+      authors: get_book_author(parsed_page),
+      description: get_book_description(parsed_page),
+      prices: get_book_prices(parsed_page)
+    }
+  end
+
+  defp get_book_prices(parsed_page),
+    do:
+      parsed_page
+      |> Floki.find(".prices > .price")
+      |> Floki.text(deep: false)
+      |> String.split()
+      |> Enum.map(&String.trim/1)
+      |> Enum.reject(fn price -> price == "$0.00" || price == "FREE" end)
+      |> then(&if &1 == [], do: :not_for_sale, else: &1)
+
+  defp get_book_description(parsed_page),
+    do:
+      parsed_page
+      |> Floki.find("[name*='about-the-book']+p")
+      |> Floki.text(deep: false)
+      |> String.trim()
+
+  defp get_book_author(parsed_page),
+    do:
+      parsed_page
+      |> Floki.find(".product-authors")
+      |> Floki.text(deep: false)
+      |> String.split([",", "and"])
+      |> Enum.map(&String.trim/1)
+      |> Enum.reject(&Kernel.==(&1, ""))
+
+  defp get_book_title(parsed_page),
+    do:
+      parsed_page
+      |> Floki.find(".product-title")
+      |> Floki.text(deep: false)
+      |> String.trim()
 end
